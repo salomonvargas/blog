88263ebc35ddec3543c068e978327c2faac3f958
Salomón Vargas <salomonvargas@outlook.com>
2020-12-10 15:20:23 -0600

    .

--- shalo-app/versions/1.0.0/win/shalo.ps1
+++ shalo-app/versions/1.0.0/win/shalo.ps1
@@ -1,10 +1,13 @@
 #Requires -RunAsAdministrator
 $instalarionPath = "$Env:Programfiles\Shalo"
 New-Item -ItemType Directory -Path $instalarionPath -Force -Verbose
-Set-Location -Path $instalarionPath
-Get-ChildItem * -Recurse | Remove-Item
-Invoke-WebRequest -Uri "https://salomonvargas.github.io/shalo-universe/shalo-app/shalo-app/versions/1.0.0/win/shalo.exe" -OutFile "$instalarionPath\shalo.exe"
+Remove-Item -Path "$instalarionPath\shalo.exe" -Force
+Invoke-WebRequest -Uri "https://salomonvargas.github.io/shalo-universe/shalo-app/versions/1.0.0/win/shalo.exe" -OutFile "$instalarionPath\shalo.exe"
 
 if(! $env:Path -like "*$instalarionPath*"){
     $env:Path += ";$instalarionPath" 
 }
+
+Write-Host "Congratularions!, Shalo app is now installed"
+Write-Host " "
+Write-Host "Please run 'Shalo -h' for get help"
