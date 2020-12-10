e0b055fec4fc5401f5295e958080637086f28ec8
Salomón Vargas <salomonvargas@outlook.com>
2020-12-10 15:58:20 -0600

    .

--- shalo-app/versions/1.0.0/linux/shalo.sh
+++ shalo-app/versions/1.0.0/linux/shalo.sh
@@ -1,6 +1,7 @@
 curl https://salomonvargas.github.io/shalo-universe/shalo-app/versions/1.0.0/linux/shalo -o /bin/shalo
+sudo chmod +x /bin/shalo
 clear;
 rm shalo.sh -f
-echo"Congratularions!, Shalo app is now installed"
+echo "Congratularions!, Shalo app is now installed"
 echo " "
-echo"Please run 'shalo -h' for get help"
+echo "Please run 'shalo -h' for get help"
