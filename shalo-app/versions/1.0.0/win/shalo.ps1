127426942997693f861348c3226e1f6b99288a8b
Salomón Vargas <salomonvargas@outlook.com>
2020-12-11 08:47:18 -0600

    .

--- shalo-app/versions/1.0.0/win/shalo.ps1
+++ shalo-app/versions/1.0.0/win/shalo.ps1
@@ -4,8 +4,12 @@ Remove-Item -Path "$instalarionPath\shalo.exe" -Force -ErrorAction Ignore -Infor
 New-Item -ItemType Directory -Path $instalarionPath -Force -InformationAction SilentlyContinue
 Invoke-WebRequest -Uri "https://salomonvargas.github.io/shalo-universe/shalo-app/versions/1.0.0/win/shalo.exe" -OutFile "$instalarionPath\shalo.exe"
 
-if(! $env:Path -like "*$instalarionPath*"){
-    $env:Path += ";$instalarionPath" 
+$oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+if(! $oldpath.IndexOf("$instalarionPath") -ge 0){
+    $oldpath += ";$instalarionPath" 
+    Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $oldpath
+
+    $ENV:PATH="$ENV:PATH;$instalarionPath"
 }
 
 Clear-Host
